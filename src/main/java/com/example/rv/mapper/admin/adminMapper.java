eea95fb49ba602d056a45ff175d514fc4ae7aeba
tangxinkang <2912528586@qq.com>
2025-05-12 10:57:18 +0800

    更新启动端口

--- src/main/java/com/example/rv/mapper/admin/adminMapper.java
+++ src/main/java/com/example/rv/mapper/admin/adminMapper.java
@@ -1,9 +1,14 @@
 package com.example.rv.mapper.admin;
 
 import com.example.rv.pojo.Admin;
+import com.example.rv.pojo.UserFeedback;
+import com.example.rv.pojo.UserQuestions;
+
+import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface AdminMapper {
@@ -26,5 +31,20 @@ public interface AdminMapper {
     @Select("SELECT COUNT(*) FROM payments WHERE payment_status = #{status}")
     Integer getPaymentCountByStatus(Integer status);
 
+    // 根据id获取问题
+    @Select("SELECT * FROM user_questions WHERE user_question_id = #{userQuestionId}")
+    UserQuestions getQuestionById(int userQuestionId);
+
+    // 更新问题
+    @Update("UPDATE user_questions SET user_question_answer = #{userQuestionAnswer}, user_question_status = #{userQuestionStatus} WHERE user_question_id = #{userQuestionId}")
+    int updateQuestion(UserQuestions question);
+
+    // 获取所有问题
+    @Select("SELECT * FROM user_questions")
+    List<UserQuestions> getQuestions();
+
+    // 获取所有反馈
+    @Select("SELECT * FROM user_feedback")
+    List<UserFeedback> getFeedbacks();
 
 }
